43d565c27445b38a21e03347720c33270129f0d8
root <elbert.bae@gmail.com>
2022-01-30 12:34:56 -0800

    Initial server

--- server/src/controllers/stocks.controller.ts
+++ server/src/controllers/stocks.controller.ts
@@ -13,26 +13,26 @@ export const getIntraday = async (req, res) => {
   const query = req.query as GetIntradayStockInput;
   const result = await intraday(query);
 
-  res.status(200).send(result);
+  res.status(200).json(result);
 };
 
 export const getDailyAdjusted = async (req, res) => {
   const query = req.query as GetIntradayStockInput;
   const result = await dailyAdjusted(query);
 
-  res.status(200).send(result);
+  res.status(200).json(result);
 };
 
 export const getWeeklyAdjusted = async (req, res) => {
   const query = req.query as GetStockInputBase;
   const result = await weeklyAdjusted(query);
 
-  res.status(200).send(result);
+  res.status(200).json(result);
 };
 
 export const getMonthlyAdjusted = async (req, res) => {
   const query = req.query as GetStockInputBase;
   const result = await monthlyAdjusted(query);
 
-  res.status(200).send(result);
+  res.status(200).json(result);
 };
